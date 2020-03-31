08d6b8b066f855944798820ed8abf031902fb602
Shingirai Chanakira <sechanakira@yahoo.com>
2020-03-31 16:23:17 +0200

    Added a controller

---
+++ src/main/java/com/github/sechanakira/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.github.sechanakira.controller;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+
+@Controller
+public class HelloController {
+
+    @Get
+    @Produces(MediaType.TEXT_PLAIN)
+    private String index() {
+        return "Hello World";
+    }
+}
